19ad5118582413fdcc55df83b99efb0b168f6ad3
Your Actual Name <Your Actual Email>
2014-05-01 17:51:12 -0700

    new mofo

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,86 @@
+class ListingsController < ApplicationController
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
+  before_filter :check_user, only: [:edit, :update, :destroy]
+
+def seller
+    @listings = Listing.where(user: current_user).order("created_at DESC")
+  end
+
+  # GET /listings
+  # GET /listings.json
+  def index
+    @listings = Listing.where(user: current_user).order("created_at DESC")
+  end
+
+  # GET /listings/1
+  # GET /listings/1.json
+  def show
+  end
+
+  # GET /listings/new
+  def new
+    @listing = Listing.new
+  end
+
+  # GET /listings/1/edit
+  def edit
+  end
+
+  # POST /listings
+  # POST /listings.json
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.user_id = current_user.id
+    respond_to do |format|
+      if @listing.save
+        format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @listing }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @listing.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /listings/1
+  # PATCH/PUT /listings/1.json
+  def update
+    respond_to do |format|
+      if @listing.update(listing_params)
+        format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @listing.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /listings/1
+  # DELETE /listings/1.json
+  def destroy
+    @listing.destroy
+    respond_to do |format|
+      format.html { redirect_to listings_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_listing
+      @listing = Listing.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def listing_params
+       params.require(:listing).permit(:name, :description, :price, :image)
+    end
+
+    def check_user
+      if current_user != @listing.user
+        redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
+    end 
+  end
+end
---
+++ app/models/listing.rb
@@ -0,0 +1,17 @@
+class Listing < ActiveRecord::Base
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+  else
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+                      :storage => :dropbox,
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                      :path => ":style/:id_:filename"
+  end
+
+      validates :name, :description, :price, presence: true
+      validates :price, numericality: { greater_than: 0 }
+      validates_attachment_presence :image
+
+      belongs_to :user
+      
+end
